e1d5a8fffaf1a23506507c746c7349369e0b4029
Anchlate Lee <anchlate@yahoo.com>
2017-06-14 09:20:16 +0800

    df

---
+++ LockAndUnlockScreenEvent/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LockAndUnlockScreenEvent
+//
+//  Created by Anchlate Lee on 16/7/10.
+//  Copyright © 2016年 Anchlate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LockAndUnlockScreenEvent/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LockAndUnlockScreenEvent
+//
+//  Created by Anchlate Lee on 16/7/10.
+//  Copyright © 2016年 Anchlate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
